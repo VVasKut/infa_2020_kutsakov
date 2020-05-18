cc982162fdcbfa298e3c836c93531a2ca4f305ca
Vasily Kutsakov <vasily.kutsakov@gmail.com>
2020-05-19 00:55:35 +0300

    Added practice examples (example-1.py, hatch.py) and first practice case - angry-smile.py

---
+++ lab3/angry-smile.py
@@ -0,0 +1,27 @@
+from graph import *
+
+windowSize(700, 700)
+
+penSize(2)
+penColor(0, 0, 0)
+brushColor(255, 238, 3)
+circle(300, 350, 150)
+
+brushColor(0, 0, 0)
+rectangle(225, 425, 375, 450)
+
+brushColor(250, 0, 0)
+circle(225, 300, 25)
+circle(375, 300, 20)
+brushColor(0, 0, 0)
+circle(225, 300, 10)
+circle(375, 300, 10)
+
+polygon([(150,210), (140,220),
+         (270,295), (280,285)])
+
+polygon([(450,220), (460,230),
+         (330,295), (320,285)])
+
+
+run()
